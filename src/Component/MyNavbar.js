f63e369260748461f1a8345d1c769774e9dd14a1
rohit <102724052+rohitbakde24@users.noreply.github.com>
2022-04-26 15:38:53 +0530

    Update MyNavbar.js

--- src/Component/MyNavbar.js
+++ src/Component/MyNavbar.js
@@ -11,16 +11,16 @@ const MyNavbar = () => {
     <>
       <Navbar bg="white" expand="lg">
         <Container >
-        <Navbar.Brand href="#home">CryptoCurrency</Navbar.Brand>
-      <Navbar.Toggle aria-controls="navbarScroll" />
-      <Navbar.Collapse className="justify-content-end">
-    <Nav>
-        <Nav.Link as={Link} to='/' className='ps-5'>Dashboard</Nav.Link>
-        <Nav.Link as={Link} to='/news'className='ps-5' >Crypto News</Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
+          <Navbar.Brand href="#home">CryptoCurrency</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse className="justify-content-end">
+            <Nav>
+              <Nav.Link as={Link} to='/' className='ps-5'>Dashboard</Nav.Link>
+              <Nav.Link as={Link} to='/news'className='ps-5' >Crypto News</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
 
 
 <section>
